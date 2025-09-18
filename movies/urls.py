95a553e0ec966aff56a2d5d212d1a7661cc2a5d6
Richard Wan <myrst131@gmail.com>
2025-09-18 10:09:31 -0400

    feat: liking movies & liked movies page

--- movies/urls.py
+++ movies/urls.py
@@ -2,9 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='movies.index'),
-    path('<int:id>/', views.show, name='movies.show'),
-    path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
-    path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
-    path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
+    path("", views.index, name="movies.index"),
+    path("liked/", views.liked_movies, name="movies.liked"),
+    path("<int:id>/", views.show, name="movies.show"),
+    path("<int:id>/review/create/", views.create_review, name="movies.create_review"),
+    path(
+        "<int:id>/review/<int:review_id>/edit/",
+        views.edit_review,
+        name="movies.edit_review",
+    ),
+    path(
+        "<int:id>/review/<int:review_id>/delete/",
+        views.delete_review,
+        name="movies.delete_review",
+    ),
+    path("<int:id>/like/", views.toggle_like, name="toggle_like"),
 ]
+
