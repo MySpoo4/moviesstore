9b6ed9712695e29e96a84c2c8fd7403ce31c2ca1
Richard Wan <myrst131@gmail.com>
2025-09-18 11:44:25 -0400

    feat: like reviews, reviews page

--- movies/models.py
+++ movies/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField(upload_to='movie_images/')
+    image = models.ImageField(upload_to="movie_images/")
 
     def __str__(self):
-        return str(self.id) + ' - ' + self.name
+        return str(self.id) + " - " + self.name
+
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,5 +20,8 @@ class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    liked_by = models.ManyToManyField(User, related_name="liked_reviews", blank=True)
+
     def __str__(self):
-        return str(self.id) + ' - ' + self.movie.name
+        return str(self.id) + " - " + self.movie.name
+
